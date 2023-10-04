41a9e180e1b713f9d60644ca21d9e685ab03f6f9
Julián Castillo <83615462+chuli99@users.noreply.github.com>
2023-10-04 11:38:47 -0300

    ocr test funcionando en pocos casos

--- ocr-test/Scripts/activate.bat
+++ ocr-test/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Julian\Documents\Proyectos\Inteligencia-Artificial\ocr-test
+set VIRTUAL_ENV=C:\Users\Julian\Documents\Proyectos\Inteligencia-Artifical\ocr-test
 
 if not defined PROMPT set PROMPT=$P$G
 
