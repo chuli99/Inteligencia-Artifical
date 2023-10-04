3306e4e22ed24ad382927a6d4d5a25c2c0d41e0f
Julián Castillo <83615462+chuli99@users.noreply.github.com>
2023-10-04 11:21:08 -0300

    agrego ejemplo sin filtrado

--- ocr-test/main.py
+++ ocr-test/main.py
@@ -8,7 +8,7 @@ import numpy as nps
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Open image
-file = Image.open(r'C:\Users\Julian\Documents\Proyectos\Inteligencia-Artifical\ocr-test\Img\larreptil.jpg')
+file = Image.open(r'C:\Users\Julian\Documents\Proyectos\Inteligencia-Artifical\ocr-test\Img\nombre.jpg')
 
 txt = pytesseract.image_to_string(file, config='--psm 6', nice=0, output_type=pytesseract.Output.STRING )
 print(txt)
