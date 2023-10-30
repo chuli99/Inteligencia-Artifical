213813cafc5ae6a046dad238dc41c8a55fa7a8f1
Julián Castillo <83615462+chuli99@users.noreply.github.com>
2023-10-30 12:24:37 -0300

    ordeno codigo de pytesseract

--- ocr-test/tesseract-test.py
+++ ocr-test/tesseract-test.py
@@ -2,43 +2,48 @@ import cv2
 import numpy as np
 import pytesseract
 import filter 
-from spellchecker import SpellChecker
+import bardApi
 
-spell = SpellChecker(language='es')
+class PYTESSERACT():
+    def __init__(self):
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-img = cv2.imread(r'C:\Users\Julian\Documents\Proyectos\Inteligencia-Artifical\ocr-test\Img\Universidad.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#cv2.imshow('Original', img)
-
-_, result = cv2.threshold(img, 100,255, cv2.THRESH_BINARY, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
-#cv2.imshow('Threshold', result)
-cv2.waitKey(0)
-
-print("Aplicando filtrado...")
-gray = filter.get_grayscale(result)
-thresh = filter.thresholding(gray)
-opening = filter.opening(gray)
-canny = filter.canny(gray)
-
-cv2.imwrite("gray.jpg", gray)
-
-print("Aplicando OCR...")
-txt = pytesseract.image_to_string(gray, config='--psm 8 --oem 3 -l spa', nice=0, output_type=pytesseract.Output.STRING)
-print("Texto:")
-print(txt)
-
-words = txt.split()
-corrected_text = []
-for word in words:
-    corrected_word = spell.correction(word)
-    corrected_text.append(spell.correction(word))
-
-#corrected_text = ' '.join(corrected_text)
-for word in corrected_text:
-    if word == None:
-        corrected_text.remove(word)
+    def open_image(self, path):
+        img = cv2.imread(path)
+        return img
     
-#print("Texto original:", txt)
-#print("Texto corregido:", corrected_text)
+    def process_image(self, img):
+        _, result = cv2.threshold(img, 100,255, cv2.THRESH_BINARY, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
+        cv2.waitKey(0)
+        gray = filter.get_grayscale(result)
+        cv2.imwrite("result.jpg", gray)
+        return gray
+    
+    def get_text(self,img):
+        txt = pytesseract.image_to_string(img, config='--psm 11', nice=0, output_type=pytesseract.Output.STRING )
+        return txt
+    
+    def fix_text(self,txt):
+        correctedText = bardApi.get_response(txt)
+        print("Texto corregido:\n")
+        print(correctedText)
+        
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    def main(self):
+        imagen = self.open_image(r'C:\Users\Julian\Documents\Proyectos\Inteligencia-Artifical\ocr-test\Img\poema.jpg')
+        imagenProcesada = self.process_image(imagen)
+        texto = self.get_text(imagenProcesada)
+        print(texto)
+        #self.fix_text(texto)
+
+if __name__ == "__main__":
+    test = PYTESSERACT()
+    test.main()
+
+
+
+
+
+
+    
+
